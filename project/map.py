18b8e8c8c10fce3ce707c59de69740a871fd3508
leonoverweel <l.overweel@gmail.com>
2018-11-12 16:05:45 +0000

    Refactor plotting code

--- project/map.py
+++ project/map.py
@@ -5,23 +5,31 @@ import matplotlib.pyplot as plt
 
 import cartopy.crs as ccrs
 
+
+def plot_netherlands(ax):
+    df = geopandas.read_file('data/ne_10m_admin_0_countries')
+    poly = df.loc[df['ADMIN'] == 'Netherlands']['geometry'].values[0]
+
+    ax.add_geometries(
+        poly, 
+        crs=ccrs.PlateCarree(), 
+        facecolor='0.9'
+    )
+
+# Set up axes
 plt.figure(figsize=(4,4.5))
-
-df = geopandas.read_file('data/ne_10m_admin_0_countries')
-poly = df.loc[df['ADMIN'] == 'Netherlands']['geometry'].values[0]
-
 ax = plt.axes(projection=ccrs.PlateCarree(), frameon=False)
-plt.gca().outline_patch.set_visible(False)
 
-ax.add_geometries(
-    poly, 
-    crs=ccrs.PlateCarree(), 
-    facecolor='0.9'
-)
+# Plot stuff
+plot_netherlands(ax)
+
+# Make figure pretty
+plt.gca().outline_patch.set_visible(False)
+plt.tight_layout()
 
 ax.set_extent([3.1, 7.5, 50.6, 53.7], crs=ccrs.PlateCarree())
 ax.set_aspect('auto')
 
-plt.tight_layout()
+# Save figure
 plt.savefig('data/test.pdf')
 plt.show()
