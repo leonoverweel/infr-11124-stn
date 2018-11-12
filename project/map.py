a1610a54af7fa3aefbcf773d544eca0a45d0f856
leonoverweel <l.overweel@gmail.com>
2018-11-12 16:29:28 +0000

    Implement showing stations on map

--- project/map.py
+++ project/map.py
@@ -1,8 +1,9 @@
+import csv
+
 from cartopy.io import shapereader
 import numpy as np
 import geopandas
 import matplotlib.pyplot as plt
-
 import cartopy.crs as ccrs
 
 
@@ -13,8 +14,31 @@ def plot_netherlands(ax):
     ax.add_geometries(
         poly, 
         crs=ccrs.PlateCarree(), 
-        facecolor='0.9'
+        facecolor='0.8'
     )
+
+
+def plot_stations(ax):
+    longs = []
+    lats = []
+
+    with open('data/stations.csv') as stations_file:
+        station_reader = csv.reader(stations_file)
+        next(station_reader, None)
+
+        for station in station_reader:
+            longs.append(float(station[2]))
+            lats.append(float(station[3]))
+
+    ax.scatter(
+        np.array(lats), 
+        np.array(longs), 
+        c='k',
+        s=10,
+        transform=ccrs.PlateCarree(), 
+        zorder=2
+    )
+
 
 # Set up axes
 plt.figure(figsize=(4,4.5))
@@ -22,6 +46,7 @@ ax = plt.axes(projection=ccrs.PlateCarree(), frameon=False)
 
 # Plot stuff
 plot_netherlands(ax)
+plot_stations(ax)
 
 # Make figure pretty
 plt.gca().outline_patch.set_visible(False)
